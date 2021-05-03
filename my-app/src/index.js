4aaa08de47bba2b31bcd712bedeac2c5a644a9d1
Samantha <ramakerssamantha1@gmail.com>
2021-05-03 17:21:31 +0200

    issue fixed

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React,{useState} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './style.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import axios from "axios";
@@ -17,7 +17,7 @@ const CountryWeather = () => {
       url: `http://api.openweathermap.org/data/2.5/weather?q=Helsinki,fin&APPID=9c8b4d791758b9ad70bf345c4df2e51a` 
     })
     // les curlyB are in the then ()
-    Then((response) => {                       
+    .then((response) => {                       
       console.log(response.data);
   })
   .catch((error) => {
@@ -25,10 +25,17 @@ const CountryWeather = () => {
   });
 };
 
-return(
 
-<input type="text" value={city} onChange= {(e) => setCity (e.target.value)}/>
-);
+  return(
+    <div>
+
+    <input type="text" value={city} placeholder="enter the city" onChange= {(e) => setCity (e.target.value)}/>
+
+    <input type="text" value={country}  placeholder="Enter the country" onChange={(e)=> setCountry (e.target.value)} />
+
+    </div>
+)
+}
 
 ReactDOM.render(
   <React.StrictMode>
