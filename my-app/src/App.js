38fc07a872cbab4521d63d1ee20b483d164cd5a1
Samantha <ramakerssamantha1@gmail.com>
2021-05-05 13:04:39 +0200

    front update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import {getIndex} from './index';
+import  React, { useState,useEffect } from 'react';
 
+
+//creating my flags:
 function App() {
-  return (
+  const [index, setIndex] = useState(null);
+  const [city, setCity] = useState('Kigali');
+  const [laoding, setLaoding] = useState(false);
+
+//creating my function try-catch : 
+const getData = async () => {
+  try {
+    const data = await getIndex(city);
+    setIndex(data);
+    console.log (data);
+  } catch (error) {
+   console.log(error.message);
+  }
+}
+
+// creating my useEffet:
+useEffect(() => {
+    getData();
+},[]);
+
+return (
     <div className="App">
+      <div className="card">
+        <h2 className ="title"><i class="fab fa-cloudversify"></i>Forecast</h2>
+        <div className="search-form">
+          <input type="text" onChange={(e) => setCity(e.target.value)} placeholder="enter your city name"/>
+          <button type="button" onClick={() => getData()}>Search</button>
+      </div>
+      <div className="main-container">
+        <h4>Live weather condition</h4>
+        <div className="weather-icon">
+          <i className="fa fa-sun"></i>
+        </div>
+        <h3>Sunny</h3>
+        <div className="temperature">
+          <h1>25&deg;C</h1>
+        </div>
+        <div className="location">
+          <h3><i className="fa fa-street-view"></i>Kigali | Rwanda</h3>
+        </div>
+      </div>
+      </div>
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
+        {/* <p>
           <code>Hi there!</code> 
-        </p>
+        </p> */}
         {/* <a
           className="App-link"
           href="https://reactjs.org"
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,39 +3,19 @@ import ReactDOM from 'react-dom';
 import './style.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import axios from "axios";
+import axios from 'axios';
 
+const baseUrl = 'api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}';
+const apiKey = '9c8b4d791758b9ad70bf345c4df2e51a';
 
-const CountryWeather = () => {
-  const [temperature, setTemperature] = useState("");
-  const [city, setCity] = useState("Helsinki");
-  const [country, setCountry] = useState ("FIN");
-
-  const getweatherData = (city,country) =>{
-    axios({
-      method : "GET",
-      url: `http://api.openweathermap.org/data/2.5/weather?q=Helsinki,fin&APPID=9c8b4d791758b9ad70bf345c4df2e51a` 
-    })
-    // les curlyB are in the then ()
-    .then((response) => {                       
-      console.log(response.data);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-};
-
-
-  return(
-    <div>
-
-    <input type="text" value={city} placeholder="enter the city" onChange= {(e) => setCity (e.target.value)}/>
-
-    <input type="text" value={country}  placeholder="Enter the country" onChange={(e)=> setCountry (e.target.value)} />
-
-    </div>
-)
+export  const getIndex = async(cityname) => {
+  try{
+    const {data} = await axios.get(baseUrl + `q=${cityname}&appid=${apiKey}`)
+  }catch(error){
+    throw error;
+  }
 }
+
 
 ReactDOM.render(
   <React.StrictMode>
